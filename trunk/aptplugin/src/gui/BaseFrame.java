2ce39419345bfa4936df5540b3ef66afb9fa3f97
apico <apico@23aac120-1128-46ae-97c9-9e165d6aae65>
2010-08-23 02:06:01 +0000

    fixing headers

--- trunk/aptplugin/src/gui/BaseFrame.java
+++ trunk/aptplugin/src/gui/BaseFrame.java
@@ -1,15 +1,23 @@
+/*******************************************************************************
+ * Copyright 2010 Alexander Pico
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 /*
  * BaseFrame.java
  * Created on 4 Jun, 2010, 5:55:55 PM
  */
 package gui;
 
-import apt.StreamReaderThread;
-import data.ArrayData;
-import data.FileLocator;
-import downloader.SequenceDownloader;
-import extractor.GZIPExtractor;
-import extractor.ZipExtractor;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
@@ -21,9 +29,17 @@ import java.io.FileInputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Properties;
+
 import javax.swing.JOptionPane;
+
 import plugin.APTEvent;
 import plugin.PluginMain;
+import apt.StreamReaderThread;
+import data.ArrayData;
+import data.FileLocator;
+import downloader.SequenceDownloader;
+import extractor.GZIPExtractor;
+import extractor.ZipExtractor;
 
 /**
  *
